a46002a6ecd317fc097856b19b369f72cde1165b
J. Soumya Sidharth <sidharthsoumya@gmail.com>
2021-04-15 23:59:28 +0530

    Comments removed from camera operations

--- windowsOperations/camera.py
+++ windowsOperations/camera.py
@@ -6,9 +6,7 @@ def takePhoto():
     strt = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     cam = cv2.VideoCapture(0)
     s, img = cam.read()
-    if s:    # frame captured without any errors
-        # namedWindow("cam-test", CV_WINDOW_AUTOSIZE)
-        # imshow("cam-test",img)
+    if s:
         cv2.destroyWindow("cam-test")
         cv2.imwrite("C:\\Users\\haPPy\\OneDrive\\Pictures\\Camera Roll\\IMG"+strt+'.jpg',img)
 def takeScreenshot():
